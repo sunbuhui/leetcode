0dc744caebfea5c29e0c137cdd1ccc113fa26bc1
gjt <gjt@gjtdeMacBook-Air.local>
2021-03-26 13:46:35 +0800

    Initial commit

---
+++ src/main/java/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,34 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+class RemoveDuplicatesfromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode fakeHead = new ListNode();
+        ListNode tail = fakeHead;
+        while (head != null) {
+            tail.next = head;
+            tail = head;
+            while (head.next != null && head.next.val == head.val) {
+                head = head.next;
+            }
+            head = head.next;
+        }
+        tail.next = null;
+        return fakeHead.next;
+    }
+}
